a5ddd404cb53b5acf5fa549199c01231416e1fce
Yann Ramin <atrus@stackworks.net>
2009-08-02 17:50:12 -0700

    Add iterator constructs

--- reader.cpp
+++ reader.cpp
@@ -7,16 +7,22 @@
 namespace EveCache {
 
     CacheFile::CacheFile(const std::string &filename)  : contents(0), length(0), valid(false),
-                                                         filename(filename), pos(0)
+                                                         filename(filename)
     {
 
     }
 
     CacheFile::CacheFile(const CacheFile&rhs) : length(rhs.length), valid(rhs.valid), 
-                                          filename(rhs.filename), pos(rhs.pos) 
+                                          filename(rhs.filename)
     {
         contents = new unsigned char[length];
         memcpy(contents, rhs.contents, length);
+    }
+
+    CacheFile::~CacheFile()
+    {
+        if (contents != 0)
+            delete contents;
     }
 
     bool CacheFile::readFile()
@@ -44,13 +50,96 @@ namespace EveCache {
         return length;
     }
 
-    bool CacheFile::atEnd() const
+    CacheFile_Iterator CacheFile::begin() const
     {
-        if (pos >= length)
+        return CacheFile_Iterator(this, 0, getLength());
+    }
+
+    CacheFile_Iterator CacheFile::end() const
+    {
+        return CacheFile_Iterator(this, length + 1, getLength());
+    }
+    
+    unsigned char CacheFile::byteAt(int pos) const
+    {
+        if (pos < length)
+            return contents[pos];
+        return 0xFF;
+    }
+    
+
+/**
+ * Iterator 
+ */
+
+    CacheFile_Iterator::CacheFile_Iterator(CacheFile const *cf, int position, int valid_length) :
+        cacheFile(cf), pos(position), limit(valid_length)
+    {
+    }
+
+    CacheFile_Iterator::~CacheFile_Iterator()
+    {
+    }
+
+    CacheFile_Iterator& CacheFile_Iterator::operator=(const CacheFile_Iterator& rhs)
+    {
+        pos = rhs.pos;
+        limit = rhs.limit;
+        cacheFile = rhs.cacheFile;
+        return *this;
+    }
+
+    bool CacheFile_Iterator::operator==(const CacheFile_Iterator& rhs) const
+    {
+        if (rhs.pos == pos && rhs.cacheFile == cacheFile)
             return true;
         return false;
     }
 
-    
+    bool CacheFile_Iterator::operator!=(const CacheFile_Iterator& rhs) const
+    {
+        return !((*this) == rhs);
+    }
+
+    int CacheFile_Iterator::peekInt() const
+    {
+        return 0;
+    }
+
+    char CacheFile_Iterator::peekChar() const
+    {
+        return '0';
+    }
+
+    float CacheFile_Iterator::peekFloat() const
+    {
+        return 0.0;
+    }
+
+    double CacheFile_Iterator::peekDouble() const
+    {
+        return 0.0;
+    }
+
+    std::string CacheFile_Iterator::peekString(int len) const
+    {
+        return std::string("");
+    }
+
+    bool CacheFile_Iterator::advance(int len)
+    {
+        pos += len;
+        if (len <= limit)
+            return true;
+        return false;
+    }
+
+    CacheFile_Iterator& CacheFile_Iterator::operator+=(int len)
+    {
+        advance(len);
+        return *this;
+    }
+
+
 
 };
--- reader.hpp
+++ reader.hpp
@@ -5,25 +5,51 @@
 
 namespace EveCache {
 
+    class CacheFile_Iterator;
+
     class CacheFile {
     public:
         CacheFile(const std::string &filename);
         CacheFile(const CacheFile&);
+        ~CacheFile();
         bool readFile();
         int getLength() const;
-        bool atEnd() const;
+        CacheFile_Iterator end() const;
+        CacheFile_Iterator begin() const;
 
         unsigned char byteAt(int) const;
-        unsigned char peekByte() const;
-        unsigned int peekInt() const;
 
     private:
         unsigned char* contents;
         int length;
         bool valid;
         std::string filename;
+    };
 
+    class CacheFile_Iterator { // This does not adhere to the iterator protocol, yet
+    public:
+        CacheFile_Iterator(CacheFile const*, int position, int valid_length);
+        ~CacheFile_Iterator();
+
+        CacheFile_Iterator& operator=(const CacheFile_Iterator& other);
+        bool operator==(const CacheFile_Iterator& rhs) const;
+        bool operator!=(const CacheFile_Iterator& rhs) const;
+        
+        int peekInt() const;
+        char peekChar() const;
+        float peekFloat() const;
+        double peekDouble() const;
+        std::string peekString(int len) const;
+
+        bool advance(int len);
+        //CacheFile_Iterator operator+(CacheFile_Iterator& lhs, const int len) const;
+        CacheFile_Iterator& operator+=(int len);
+
+    private:
+        CacheFile const *cacheFile;
         int pos;
+        int limit;
+        
     };
 };
 
