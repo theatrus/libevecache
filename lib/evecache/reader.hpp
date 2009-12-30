28adfb9874e194f274a9a4be0b9ff7edf3a7b5bc
Yann Ramin <atrus@stackworks.net>
2009-12-29 23:18:25 -0800

    Fix non-existant URL

--- lib/evecache/reader.hpp
+++ lib/evecache/reader.hpp
@@ -15,7 +15,7 @@
 // License along with this library; if not, write to the Free Software
 // Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 //
-// http://www.stackfoundry.com/libevecache
+// http://dev.eve-central.com/libevecache/
 // http://gitorious.org/libevecache
 
 #ifndef _EC_READER_H_
--- lib/exceptions.cpp
+++ lib/exceptions.cpp
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
-// http://www.stackfoundry.com/libevecache
+// http://dev.eve-central.com/libevecache/
 // http://gitorious.org/libevecache
 
 #include "evecache/exceptions.hpp"
